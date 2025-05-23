f576620cafc12f2941656148222286d7ac980596
LePhanHaiLong <longlph.23it@gmail.com>
2025-05-24 01:46:11 +0700

    project complete

--- src/components/Content/Project.tsx
+++ src/components/Content/Project.tsx
@@ -1,8 +1,27 @@
 import { Button, Modal } from 'antd';
 import { useState } from 'react';
+import { BsArrowRight } from "react-icons/bs";
+import { FaReact } from "react-icons/fa";
+import { FaNodeJs } from "react-icons/fa";
+import { SiNestjs } from "react-icons/si";
+interface IDetail {
+  description: string,
+  frontend: string,
+  backend: string,
+  member: number,
+  role: string,
+  demo: string,
+  github: string
+}
+interface IDataProject {
+  image: JSX.Element,
+  title: string,
+  shortDescription: string,
+  detail: IDetail
+}
 const Project = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-
+  const [dataDetail, setDataDetail] = useState<IDataProject | null>(null);
   const showModal = () => {
     setIsModalOpen(true);
   };
@@ -13,64 +32,108 @@ const Project = () => {
 
   const handleCancel = () => {
     setIsModalOpen(false);
+    setDataDetail(null)
   };
 
-  const dataProjects = [
+
+
+  const dataProjects: IDataProject[] = [
     {
-      image: "img/svg/camera-diaphragm.svg",
-      title: "ReactJS",
-      shortDescription: "Web design is a similar process of creation, with the intention of presenting the content on electronic pages ...",
+      image: <FaReact
+        style={{
+          width: "50px",
+          height: "50px",
+        }}
+        color={'#2bebfd'}
+      />,
+      title: "Website bán sách kết hợp mạng xã hội",
+      shortDescription: "Website bán sách kết hợp mạng xã hội, nơi người dùng có thể đăng bài viết, bình luận và tương tác với nhau",
       detail: {
-        description: "", // Miêu tả dự án làm gì
-        technology: "", // Công nghệ sử dụng
-        member: "", // Thành viên tham gia
-        role: "", // Vai trò
-        demo: "", // Link demo, link deploy
-        github: "" // Link github
+        description: "Website bán sách kết hợp mạng xã hội, nơi người dùng có thể đăng bài viết, bình luận và tương tác với nhau",
+        frontend: "ReactJS, Redux Toolkit, Ant Design",
+        backend: "NodeJS (ExpressJS), MongoDB",
+        member: 1,
+        role: "Developer",
+        demo: "https://longtalk.vercel.app/",
+        github: "https://github.com/longlephanhai/education_fe"
       }
     },
     {
-      image: "img/svg/camera-diaphragm.svg",
-      title: "ReactJS",
-      shortDescription: "Web design is a similar process of creation, with the intention of presenting the content on electronic pages ...",
+      image: <FaNodeJs
+        style={{
+          width: "50px",
+          height: "50px",
+        }}
+        color={'#7CB701'}
+      />,
+      title: "Quản lý kho hàng",
+      shortDescription: "Quản lý kho hàng là một ứng dụng web giúp người dùng quản lý hàng hóa trong kho, theo dõi số lượng, giá trị và tình trạng của hàng hóa.",
       detail: {
-        description: "", // Miêu tả dự án làm gì
-        technology: "", // Công nghệ sử dụng
-        member: "", // Thành viên tham gia
-        role: "", // Vai trò
-        demo: "", // Link demo, link deploy
-        github: "" // Link github
+        description: "Quản lý kho hàng là một ứng dụng web giúp người dùng quản lý hàng hóa trong kho, theo dõi số lượng, giá trị và tình trạng của hàng hóa.",
+        frontend: "ReactJS, Redux Toolkit, Ant Design",
+        backend: "NodeJS (ExpressJS), MongoDB",
+        member: 1,
+        role: "Developer",
+        demo: "https://product-manager-lemon.vercel.app/",
+        github: "https://github.com/longlephanhai/product-manager"
       }
     },
     {
-      image: "img/svg/camera-diaphragm.svg",
-      title: "ReactJS",
-      shortDescription: "Web design is a similar process of creation, with the intention of presenting the content on electronic pages ...",
+      image: <SiNestjs
+        style={{
+          width: "50px",
+          height: "50px",
+        }}
+        color={'#D9224C'}
+      />,
+      title: "Website hổ trợ học tiếng anh với AI",
+      shortDescription: "Website hổ trợ học tiếng anh với AI, nơi người dùng có thể học từ vựng, ngữ pháp và luyện nghe nói với AI",
       detail: {
-        description: "", // Miêu tả dự án làm gì
-        technology: "", // Công nghệ sử dụng
-        member: "", // Thành viên tham gia
-        role: "", // Vai trò
-        demo: "", // Link demo, link deploy
-        github: "" // Link github
+        description: "Website hổ trợ học tiếng anh với AI, nơi người dùng có thể học từ vựng, ngữ pháp và luyện nghe nói với AI",
+        frontend: "ReactJS, Redux Toolkit, Ant Design",
+        backend: "NodeJS (NestJS), MongoDB",
+        member: 1,
+        role: "Developer",
+        demo: "https://longtalk.vercel.app/",
+        github: "https://github.com/longlephanhai/education_fe"
       }
     }
   ]
   return (
     <>
-      <Button type="primary" onClick={showModal}>
-        Open Modal
-      </Button>
       <Modal
-        title="Basic Modal"
+        title={dataDetail && dataDetail.title ? `Dự án: ${dataDetail.title}` : ""}
         open={isModalOpen}
         onOk={handleOk}
         onCancel={handleCancel}
+        footer={null}
+        maskClosable={false}
       >
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+        {
+          dataDetail && (
+            <ul>
+              <li>Miêu tả: {dataDetail.detail.description}</li>
+              <li>Frontend: {dataDetail.detail.frontend}</li>
+              <li>Backend: {dataDetail.detail.backend}</li>
+              <li>Số lượng thành viên: {dataDetail.detail.member}</li>
+              <li>Vai trò: {dataDetail.detail.role}</li>
+              <li>
+                Demo:
+                <a href={dataDetail.detail.demo} target='_blank'>
+                  {dataDetail.detail.demo}
+                </a>
+              </li>
+              <li>
+                Github:
+                <a href={dataDetail.detail.github} target='_blank'>
+                  {dataDetail.detail.github}
+                </a>
+              </li>
+            </ul>
+          )
+        }
       </Modal>
+
       <div className="arlo_tm_section" id="projects">
         <div className="arlo_tm_services_wrap">
           <div className="container">
@@ -83,9 +146,17 @@ const Project = () => {
                 {
                   dataProjects.map((item, index) => (
                     <li key={index}>
-                      <div className="inner">
+                      <div
+                        className="inner"
+                        title='Xem chi tiết'
+                        style={{ cursor: "pointer" }}
+                        onClick={() => {
+                          setDataDetail(item)
+                          setIsModalOpen(true)
+                        }}
+                      >
                         <div className="icon">
-                          <img className="svg" src={item.image} alt="camera-diaphragm" />
+                          {item.image}
                         </div>
                         <div className="title_service">
                           <h3>{item.title}</h3>
@@ -93,11 +164,17 @@ const Project = () => {
                         <div className="text">
                           <p>{item.shortDescription}</p>
                         </div>
+                        <div className='view_detail' style={{ padding: "5px 0" }}>
+                          <span style={{ cursor: "pointer" }}>
+                            <BsArrowRight />
+                            &nbsp;
+                            Xem chi tiết
+                          </span>
+                        </div>
                       </div>
                     </li>
                   ))
                 }
-
               </ul>
             </div>
           </div>
